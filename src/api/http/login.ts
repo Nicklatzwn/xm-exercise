c6461776850c1a3008b30d88c405200ba902a84b
n.latzonis <n.latzonis@atc.gr>
2023-03-26 00:26:15 +0200

    Remove remember field/value from login post api

--- src/api/http/login.ts
+++ src/api/http/login.ts
@@ -3,5 +3,7 @@ import { AxiosResponse } from 'axios';
 import { ILoginRequest } from 'interfaces/ILoginRequest';
 import { ILoginResponse } from 'interfaces/ILoginResponse';
 
-export const loginApi = (data: ILoginRequest): Promise<AxiosResponse<ILoginResponse>> =>
-  httpApi.post('login', { ...data });
+export const loginApi = (data: ILoginRequest): Promise<AxiosResponse<ILoginResponse>> => {
+  const { name, password } = data;
+  return httpApi.post('login', { name, password });
+};
